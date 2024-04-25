1d87af2f4971018d3a02a25fb084fcd18273308c
Maks <uselessmail11111@gmail.com>
2024-04-25 09:42:38 +0200

    Fix coide for BLE server

--- carESPcodeCOMBINED/carESPcodeCOMBINED.ino
+++ carESPcodeCOMBINED/carESPcodeCOMBINED.ino
@@ -1,9 +1,12 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+// #include <LiquidCrystal_I2C.h>
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6)
 const int potPin = 34;
@@ -14,7 +17,7 @@ int potValue = 0;
 int mode = 0;
 int newmode = 0;
 //-----------------------------------ESP NOW-----------------------------------------
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+// LiquidCrystal_I2C lcd(0x27, 16, 2);
 uint8_t data;
 
 
@@ -50,9 +53,9 @@ void bleSetup() {
     Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
-bool isBLEInit() {
-  return pServer != NULL false ? true;
-}
+// bool isBLEInit() {
+//   return pServer != NULL false ? true;
+// }
 
 void bleLoop() {
   std::string val = pCharacteristic->getValue();
@@ -70,14 +73,14 @@ void OnDataRecv(const uint8_t* mac, const uint8_t* incomingData, int len) {
 
 void espnowsetup() {
   Serial.println("espnowsetup");
-  lcd.begin();
+  // lcd.begin();
   delay(500);
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello");
-  lcd.setCursor(0, 1);
-  lcd.print("World");
-  lcd.display();
+  // lcd.backlight();
+  // lcd.setCursor(0, 0);
+  // lcd.print("Hello");
+  // lcd.setCursor(0, 1);
+  // lcd.print("World");
+  // lcd.display();
 
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
@@ -98,167 +101,169 @@ void espnowloop() {
   Serial.println("espnowloop");
 }
 
-//------------------------------------MQTT-------------------------------------------
+// //------------------------------------MQTT-------------------------------------------
 
-/****** WiFi Connection Details *******/
-const char* ssid = "AndroidAP1063";
-const char* password = "alamakota123";
+// /****** WiFi Connection Details *******/
+// const char* ssid = "AndroidAP1063";
+// const char* password = "alamakota123";
 
-/******* MQTT Broker Connection Details *******/
-const char* mqtt_server = "dfb0ec72cc864eddaee0fe147972f4af.s1.eu.hivemq.cloud";
-const char* mqtt_username = "auto-car";
-const char* mqtt_password = "420Driving420";
-const int mqtt_port = 8883;
+// /******* MQTT Broker Connection Details *******/
+// const char* mqtt_server = "dfb0ec72cc864eddaee0fe147972f4af.s1.eu.hivemq.cloud";
+// const char* mqtt_username = "auto-car";
+// const char* mqtt_password = "420Driving420";
+// const int mqtt_port = 8883;
 
-const char* data_topic = "car_data";
-const char* latency_topic = "time/ack";
+// const char* data_topic = "car_data";
+// const char* latency_topic = "time/ack";
 
-WiFiClientSecure espClient;
+// WiFiClientSecure espClient;
 
-/**** MQTT Client Initialisation Using WiFi Connection *****/
-PubSubClient client(espClient);
+// /**** MQTT Client Initialisation Using WiFi Connection *****/
+// PubSubClient client(espClient);
 
-static const char* rootCA PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
-TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
-cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
-WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
-ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
-MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
-h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
-0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
-A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
-T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
-B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
-B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
-KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
-OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
-jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
-qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
-rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
-HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
-hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
-ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
-3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
-NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
-ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
-TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
-jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
-oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
-4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
-mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
-emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
------END CERTIFICATE-----
-)EOF";
+// static const char* rootCA PROGMEM = R"EOF(
+// -----BEGIN CERTIFICATE-----
+// MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+// TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+// cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+// WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+// ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+// MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+// h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+// 0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+// A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+// T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+// B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+// B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+// KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+// OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+// jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+// qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+// rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+// HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+// hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+// ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+// 3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+// NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+// ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+// TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+// jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+// oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+// 4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+// mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+// emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+// -----END CERTIFICATE-----
+// )EOF";
 
-void setup_wifi() {
+// void setup_wifi() {
 
-  delay(10);
-  Serial.print("\nConnecting to ");
-  Serial.println(ssid);
+//   delay(10);
+//   Serial.print("\nConnecting to ");
+//   Serial.println(ssid);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(500);
+//     Serial.print(".");
+//   }
 
-  randomSeed(micros());
-  Serial.println("\nWiFi connected\nIP address: ");
-  Serial.println(WiFi.localIP());
-}
+//   randomSeed(micros());
+//   Serial.println("\nWiFi connected\nIP address: ");
+//   Serial.println(WiFi.localIP());
+// }
 
 
-/************* Connect to MQTT Broker ***********/
-void reconnect() {
+// /************* Connect to MQTT Broker ***********/
+// void reconnect() {
 
-  // Loop until we're reconnected
-  while (!client.connected()) {
+//   // Loop until we're reconnected
+//   while (!client.connected()) {
 
-    Serial.print("Attempting MQTT connection...");
-    String clientId = "car-test";
+//     Serial.print("Attempting MQTT connection...");
+//     String clientId = "car-test";
 
-    // Attempt to connect
-    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
-      Serial.println("connected");
-      client.subscribe(data_topic, 0);
+//     // Attempt to connect
+//     if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+//       Serial.println("connected");
+//       client.subscribe(data_topic, 0);
 
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");  // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+//     } else {
+//       Serial.print("failed, rc=");
+//       Serial.print(client.state());
+//       Serial.println(" try again in 5 seconds");  // Wait 5 seconds before retrying
+//       delay(5000);
+//     }
+//   }
+// }
 
-/***** Call back Method for Receiving MQTT messages****/
-void callback(char* topic, byte* payload, unsigned int length) {
+// /***** Call back Method for Receiving MQTT messages****/
+// void callback(char* topic, byte* payload, unsigned int length) {
 
-  String incommingMessage = "";
-  for (int i = 0; i < length; i++) incommingMessage += (char)payload[i];
-  //Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
-  Serial.print((char)payload[0]);
-  client.publish(latency_topic, "ack");
-}
+//   String incommingMessage = "";
+//   for (int i = 0; i < length; i++) incommingMessage += (char)payload[i];
+//   //Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
+//   Serial.print((char)payload[0]);
+//   client.publish(latency_topic, "ack");
+// }
 
-void mqttsetup() {
-  Serial.println("mqttsetup");
-  while (!Serial) delay(10);
-  setup_wifi();
+// void mqttsetup() {
+//   Serial.println("mqttsetup");
+//   while (!Serial) delay(10);
+//   setup_wifi();
 
-  // espClient.setInsecure();
-  espClient.setCACert(rootCA);
-  client.setBufferSize(512);
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
-}
+//   // espClient.setInsecure();
+//   espClient.setCACert(rootCA);
+//   client.setBufferSize(512);
+//   client.setServer(mqtt_server, mqtt_port);
+//   client.setCallback(callback);
+// }
 
-void mqttloop() {
-  Serial.println("mqttloop");
-  if (!client.connected())
-    reconnect();  // check if client is connected
+// void mqttloop() {
+//   Serial.println("mqttloop");
+//   if (!client.connected())
+//     reconnect();  // check if client is connected
 
-  client.loop();
-  delay(10);
-}
+//   client.loop();
+//   delay(10);
+// }
 
 //------------------------------------YEAH-------------------------------------------
 
 void setup() {
-
   Serial.begin(9600);
+  bleSetup();
 }
 
 void loop() {
-  potValue = analogRead(potPin);
-  int newmode = map(potValue, 0, 4095, 1, 5);
+  // potValue = analogRead(potPin);
+  // int newmode = map(potValue, 0, 4095, 1, 5);
 
-  if (mode != newmode) {
-    mode = newmode;
-    if (mode == 1) {
-      espnowsetup();
-    }
-    if (mode == 2) {
-      mqttsetup();
-    }
-    if (mode == 3 && isBLEInit()) {
-      bleSetup();     
-    }
-  }
+  // if (mode != newmode) {
+  //   mode = newmode;
+  //   if (mode == 1) {
+  //     espnowsetup();
+  //   }
+  //   if (mode == 2) {
+  //     mqttsetup();
+  //   }
+  //   if (mode == 3 && isBLEInit()) {
+  //     bleSetup();     
+  //   }
+  // }
 
-  if (mode == 1) {
-    espnowloop();
-  }
-  if (mode == 2) {
-    mqttloop();
-  }
-  if (mode == 3) {
-    bleLoop();
-  }
+  // if (mode == 1) {
+  //   espnowloop();
+  // }
+  // if (mode == 2) {
+  //   mqttloop();
+  // }
+  // if (mode == 3) {
+  //   bleLoop();
+  // }
 
-  delay(100);
+  bleLoop();
+
+  delay(10);
 }
